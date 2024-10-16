6772e6a8de6946cfbefa1e81e22a2c4a846fe583
Anna <ajasielec@student.agh.edu.pl>
2024-10-16 23:09:05 +0200

    prep to jakobian method

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ Grid readFromFile(const std::string& path) {
 	// reading nodes
 	while (std::getline(myfile, line) && line != "*Element") {
 		std::istringstream iss(line);
-		Node node{};
+		Node node;
 		if (char comma; iss >> node.id >> comma >> node.x >> comma >> node.y) {
 			grid.nodes.push_back(node);
 		}
@@ -90,55 +90,87 @@ int main() {
 	// LAB 1 - READING GRID FROM FILE //
 
 	// grid 1
-	Grid grid4x4 = readFromFile("grids\\Test1_4_4.txt");
+	// Grid grid4x4 = readFromFile("grids\\Test1_4_4.txt");
+	//
+	// std::cout << std::fixed << std::setprecision(10);
+	//
+	// std::cout << "Test 1 - 4x4 Grid:" << std::endl;
+	// // grid4x4.displayGlobalData();
+	// grid4x4.displayNodes();
+	// grid4x4.displayElements();
+	//
+	// // grid 2
+	// Grid mix_grid4x4 = readFromFile("grids\\Test2_4_4_MixGrid.txt");
+	//
+	// std::cout << "\n\nTest 2 - 4x4 MixGrid:" << std::endl;
+	// mix_grid4x4.displayNodes();
+	// mix_grid4x4.displayElements();
+	//
+	// // grid 3
+	// Grid grid31x31 = readFromFile("grids\\Test3_31_31_kwadrat.txt");
+	//
+	// std::cout << "\n\nTest 3 - 31x31 Grid:" << std::endl;
+	// grid31x31.displayNodes();
+	// grid31x31.displayElements();
+	//
 
-	std::cout << std::fixed << std::setprecision(10);
 
-	std::cout << "Test 1 - 4x4 Grid:" << std::endl;
-	// grid4x4.displayGlobalData();
-	grid4x4.displayNodes();
-	grid4x4.displayElements();
+	// // LAB 2 - GAUSS QUADRATURES METHOD
+	//
+	// std::cout << "\n\nIntegration using Gauss quadratures method." << std::endl;
+	//
+	// std::cout << std::fixed << std::setprecision(15);
+	//
+	// // integral of f(x) = 5x^2 + 3x + 6
+	// std::cout << "\nf(x) = 5x^2 + 3x + 6\n";
+	// std::cout << "\tNodes nr:\tResult:\n";
+	// auto f = [](double x) {return 5 * x * x + 3 * x + 6; };
+	//
+	// for (int n = 1; n <= 4; n++) {
+	// 	std::cout << "\t\t" << n << ": \t" << Quadrature(n).solveQuadratures(f) << std::endl;
+	// }
+	// // std::cout << "\tAccurate result: " << 15.333333333333333333333333333333 << std::endl;
+	//
+	// // integral of = 5x^2 + 3x + 6
+	// std::cout << "\nf(x) = 5x^2y^2 + 3xy + 6\n";
+	// std::cout << "\tNodes nr:\tResult:\n";
+	// auto f_xy = [](double x, double y) {return 5 * x * x * y * y + 3 * x * y + 6; };
+	//
+	// for (int n = 1; n <= 4; n++) {
+	// 	std::cout << "\t\t" << n << ": \t" << Quadrature(n).solveQuadratures(f_xy) << std::endl;
+	// }
+	// // std::cout << "\tAccurate result: " << 26.222222222222222222222222222222 << std::endl;
 
-	// grid 2
-	Grid mix_grid4x4 = readFromFile("grids\\Test2_4_4_MixGrid.txt");
+	// LAB 3 - JAKOBIAN
+	// test element
+	Node node1(1, 0, 0);
+	Node node2(2, 0.025, 0);
+	Node node3(3, 0.025, 0.025);
+	Node node4(4, 0, 0.025);
+	int nodes_id[4] = {1,2,3,4};
 
-	std::cout << "\n\nTest 2 - 4x4 MixGrid:" << std::endl;
-	mix_grid4x4.displayNodes();
-	mix_grid4x4.displayElements();
+	Element element(1, nodes_id);
+	element.display();
+	node1.display();
+	node2.display();
+	node3.display();
+	node4.display();
 
-	// grid 3
-	Grid grid31x31 = readFromFile("grids\\Test3_31_31_kwadrat.txt");
+	Grid mygrid;
+	mygrid.elements.push_back(element);
+	mygrid.nodes.push_back(node1);
+	mygrid.nodes.push_back(node2);
+	mygrid.nodes.push_back(node3);
+	mygrid.nodes.push_back(node4);
+	mygrid.npc = 4;
 
-	std::cout << "\n\nTest 3 - 31x31 Grid:" << std::endl;
-	grid31x31.displayNodes();
-	grid31x31.displayElements();
 
-	std::cout << std::fixed << std::setprecision(5);
+	// completing table
+	ElemUniv elem_univ(mygrid.npc);
+	elem_univ.display();
 
+	// creating jakobian
 
-	// LAB 2 - GAUSS QUADRATURES METHOD
-
-	std::cout << "\n\nIntegration using Gauss quadratures method." << std::endl;
-
-	// integral of f(x) = 5x^2 + 3x + 6
-	std::cout << "\nf(x) = 5x^2 + 3x + 6\n";
-	std::cout << "\tNodes nr:\tResult:\n";
-	auto f = [](double x) {return 5 * x * x + 3 * x + 6; };
-
-	for (int n = 1; n <= 4; n++) {
-		std::cout << "\t\t" << n << ": \t" << Quadrature(n).solveQuadratures(f) << std::endl;
-	}
-	std::cout << "\tAccurate result: " << 15.3333333333 << std::endl;
-
-	// integral of = 5x^2 + 3x + 6
-	std::cout << "\nf(x) = 5x^2y^2 + 3xy + 6\n";
-	std::cout << "\tNodes nr:\tResult:\n";
-	auto f_xy = [](double x, double y) {return 5 * x * x * y * y + 3 * x * y + 6; };
-
-	for (int n = 1; n <= 4; n++) {
-		std::cout << "\t\t" << n << ": \t" << Quadrature(n).solveQuadratures(f_xy) << std::endl;
-	}
-	std::cout << "\tAccurate result: " << 26.22222222222 << std::endl;
 
 	return 0;
 }
